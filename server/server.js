7248fcae460b0d51b38ede409bda4567cd2c7753
SuswethaK <kollurususwetha@gmail.com>
2024-04-19 17:21:04 -0500

    First commit

---
+++ server/server.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+require("dotenv").config({path:"./config/.enviro"})
+const colors = require("colors");
+const userRoutes = require("./Routes/userRoutes");
+const chatRoutes = require("./Routes/chatRoutes");
+const messageRoutes = require("./Routes/messageRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const cors = require("cors");
+
+
+const app = express();
+dotenv.config();
+connectDB();
+app.use(express.json()); //to accept json from frontend
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("API running successfully");
+});
+
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 4000;
+const server = app.listen(PORT, () =>
+  console.log(`Server started on port ${PORT}`.yellow.bold)
+);
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("connected to socket.io");
+
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    console.log(userData._id);
+    socket.emit("connected");
+  });
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room:" + room);
+  });
+
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+
+    
+  });
+   socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+   });
+});
